d0b4da7269f40fc2aa5a3b736ea07a59f95e8a26
rolaman <rolamanbuss@yandex.ru>
2023-12-21 17:27:39 +0300

    master: producer start

--- src/handler/proposal/proposal.ts
+++ src/handler/proposal/proposal.ts
@@ -6,10 +6,12 @@ import {RequestEntity, RequestStatus} from "../../repository/request";
 export async function getProposals(ctx: Application.ParameterizedContext) {
     const userInfo = decodeJwt(ctx.request)
     const status = ctx.query.status;
+    console.log(`Proposals ${status} ${userInfo.id}`)
     if (status == 'processing') {
         const requests = await dbClient<RequestEntity>('request')
             .where('status', RequestStatus.PENDING)
             .where('assignedId', userInfo.id)
+        console.log(`Proposals ${requests}`)
         ctx.body = requests.map(r => {
             return {
                 statement_key: r.statementId,
