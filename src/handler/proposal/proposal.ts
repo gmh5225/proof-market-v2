3e10e46694a171e09966ec6cb81a7dccd8696159
rolaman <rolamanbuss@yandex.ru>
2023-12-21 16:55:23 +0300

    master: producer start

--- src/handler/proposal/proposal.ts
+++ src/handler/proposal/proposal.ts
@@ -1,6 +1,21 @@
 import Application from 'koa'
+import {dbClient} from "../../db/client";
+import {decodeJwt} from "../../service/user/hash";
+import {RequestEntity, RequestStatus} from "../../repository/request";
 
 export async function getProposals(ctx: Application.ParameterizedContext) {
-	// TODO: after mvp
-	ctx.status = 404
+	const userInfo = decodeJwt(ctx.request)
+	const status = ctx.query.status;
+	if (status == 'processing') {
+		const requests = await dbClient<RequestEntity>('request')
+			.where('status', RequestStatus.PENDING)
+			.where('assignedId', userInfo.id)
+		return requests.map(r => {
+			return {
+				statement_key: r.statementId,
+
+			}
+		})
+	}
+	return []
 }
